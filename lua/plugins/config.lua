cc8cb57df3110c6544bec3ce23a1169c27df86a1
Serhii <sergey.crisp@gmail.com>
2025-09-24 11:39:43 +0300

    add todos

--- lua/plugins/config.lua
+++ lua/plugins/config.lua
@@ -1,7 +1,9 @@
+-- TODO: fix colorscheme
 -- TODO: autocomplete with tab
 -- TODO: tab code completion
 -- TODO: language support
 -- TODO: test this on real project - just pull open source thing and try to change
+-- TODO: for golang and ts - configure lsp, plugins for autoimport, etc.
 return {
   {
     "projekt0n/github-nvim-theme",
@@ -40,40 +42,13 @@ return {
 
   {
     "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "bash",
-        "html",
-        "javascript",
-        "json",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "query",
-        "regex",
-        "tsx",
-        "typescript",
-        "vim",
-        "yaml",
-      },
-    },
-  },
-
-  -- since `vim.tbl_deep_extend`, can only merge tables and not lists, the code above
-  -- would overwrite `ensure_installed` with the new value.
-  -- If you'd rather extend the default config, use the code below instead:
-  {
-    "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
-      -- add tsx and treesitter
       vim.list_extend(opts.ensure_installed, {
         "tsx",
         "typescript",
       })
     end,
   },
-
   {
     "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
@@ -89,15 +64,13 @@ return {
   -- add jsonls and schemastore packages, and setup treesitter for json, json5 and jsonc
   { import = "lazyvim.plugins.extras.lang.json" },
 
-  -- add any tools you want to have installed below
   {
     "mason-org/mason.nvim",
     opts = {
       ensure_installed = {
-        "stylua",
-        "shellcheck",
-        "shfmt",
-        "flake8",
+        "gopls",
+        "golines",
+        "goimports",
       },
     },
   },
