5a61021b3a7582825e119535e49e36d3c93af965
Serhii <sergey.crisp@gmail.com>
2025-10-02 23:43:49 +0300

    feat(git): add blame summary

--- lua/plugins/blame.lua
+++ lua/plugins/blame.lua
@@ -3,8 +3,9 @@ return {
   event = "VeryLazy",
   opts = {
     enabled = true,
-    message_template = "<date> • <author>",
+    message_template = "<date> • <author> • <summary>",
     virtual_text_column = 1,
     date_format = "%r",
+    max_commit_summary_length = 25,
   },
 }
